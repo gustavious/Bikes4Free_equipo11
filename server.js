6f6c8e9ceb1ae2a9a69a4cdd37a0ff8bca61c284
gustavious <ga.alegria10@uniandes.edu.co>
2016-10-12 03:35:07 -0500

    cambio

--- server.js
+++ server.js
@@ -23,8 +23,6 @@ app.get('*', function(req, res) {
 
 
 
-
-
 // listen (start app with node server.js) ======================================
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 console.log("App listening on port 8080");
